b6a663c57b24728795e2430919d1dd6125c4b9e5
dcsiona@gmail.com <dcsiona@gmail.com>
2024-04-09 08:54:49 -0400

    another commit

--- hash-table.js
+++ hash-table.js
@@ -36,14 +36,51 @@ class HashTable {
 
   insertNoCollisions(key, value) {
     // Your code here
+    /* Hash table insert with hash collisions*/
+    const index = this.hashMod(key);
+
+    if (this.data[index] !== null) {
+      throw new Error('Index is already occupied');
+    }
+
+    this.data[index] = new KeyValuePair(key, value);
+    this.count++;
+
+
+
+
   }
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    const index = this.hashMod(key);
+    const newPair = new KeyValuePair(key, value);
+    let current = this.data[index];
+    if (current === null) {
+      this.data[index] = newPair;
+      this.count++;
+    } else {
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newPair;
+      this.count++;
+    }
+
+
   }
 
   insert(key, value) {
     // Your code here
+    /* const loadFactor = this.count / this.capacity;
+    if (loadFactor > 0.7) {
+      this.rehash();
+    }
+    this.insertWithHashCollisions(key, value);
+ */
+
+
+
   }
 
 }
