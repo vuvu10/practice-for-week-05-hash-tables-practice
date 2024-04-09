a24389ee3be92aca458d5180aa2ca1b5633b78a9
dcsiona@gmail.com <dcsiona@gmail.com>
2024-04-09 14:38:06 -0400

    another commit

--- hash-table.js
+++ hash-table.js
@@ -65,36 +65,115 @@ class HashTable {
 
     this.count++;
 
-
-
-
   }
+
 
   insert(key, value) {
     // Your code here
     const index = this.hashMod(key);
-    let currentPair = this.data[index];
 
-    if (currentPair !== null) {
-      while (currentPair) {
-        if (currentPair.key === key) {
-          currentPair.value = value;
-          return;
-        }
-        currentPair = currentPair.next;
-      }
+    let current = this.data[index];
+
+    if (!current) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+      return;
     }
 
-    this.data[index] = new KeyValuePair(key, value);
+    if (current.key === key) {
+      current.value = value;
+      return;
+    }
+
+    while (current.next) {
+      if (current.next.key === key) {
+        current.next.value = value;
+        return;
+      }
+      current = current.next;
+    }
+
+    const newPair = new KeyValuePair(key, value);
+    newPair.next = this.data[index];
+    this.data[index] = newPair;
     this.count++;
 
+}
 
+  get(key) {
+    // Your code here
+    const index = this.hashMod(key);
 
+    let current = this.data[index];
 
+    while (current) {
+      if (current.key === key) {
+        return current.value;
+      }
+      current = current.next;
+    }
+
+    return null;
+
+  }
+
+  delete(key) {
+    // Your code here
+    const index = this.hashMod(key);
+
+    let current = this.data[index];
+
+    if (!current) {
+      return null;
+    }
+
+    if (current.key === key) {
+      this.data[index] = current.next;
+      this.count--;
+      return current.value;
+    }
+
+    while (current.next) {
+      if (current.next.key === key) {
+        const value = current.next.value;
+        current.next = current.next.next;
+        this.count--;
+        return value;
+      }
+      current = current.next;
+    }
+
+    return null;
+
+  }
+
+  loadFactor() {
+    // Your code here
+    return this.count / this.capacity;
+  }
+
+  resize() {
+    // Your code here
+    const oldData = this.data;
+    this.capacity *= 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    for (const pair of oldData) {
+      let current = pair;
+      while (current) {
+        this.insert(current.key, current.value);
+        current = current.next;
+      }
+    }
 
   }
 
 }
+
+
+
+
 
 
 module.exports = HashTable;
